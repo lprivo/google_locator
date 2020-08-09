d91a628c34527d66c4688a22c3868de7074d4044
LPrivo <privinbox@gmail.com>
2020-08-09 13:23:58 +0100

    main

---
+++ src/Location/Location.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Location.css";
+
+export const Location = ({ eventHandler }) => {
+  return (
+    <span className="location">
+      Your location:
+    </span>
+  );
+};
+
+export default Location;
---
+++ src/Main/Main.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useCallback } from "react";
+import "./Main.css";
+import Location from "../Location";
+
+export const Main = () => {
+
+  return (
+    <div className="main">
+      <Location></Location>
+    </div>
+  );
+};
+
+export default Main;
