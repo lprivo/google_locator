a22f1816fa69cdad2b1420c1328a1d3054f1d60d
LPrivo <privinbox@gmail.com>
2020-08-13 16:33:07 +0100

    map src corrected

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -2,13 +2,10 @@ import React from "react";
 import "./Map.css";
 
 export const Map = ({ API }) => {
+  const src = `https://www.google.com/maps/embed/v1/MODE?key=${API}&parameters`;
   return (
-    <iframe className="mapframe" title="Google Map" >
-      https://www.google.com/maps/embed/v1/MODE?key={API}&parameters
-    </iframe>
+    <iframe className="mapframe" title="Google Map" src={src}></iframe>
   );
 };
-
-// https://www.google.com/maps/embed/v1/MODE?key=YOUR_API_KEY&parameters
 
 export default Map;
